682c291e8875dd3446f64bdaf162577bf92cc279
Daniel Coulbourne <daniel@tighten.co>
2019-11-05 17:47:50 -0500

    WIP

--- tests/Feature/HasCustomFieldResponsesTest.php
+++ tests/Feature/HasCustomFieldResponsesTest.php
@@ -45,4 +45,47 @@ class HasCustomFieldResponsesTest extends TestCase
         $this->assertCount(1, $customFieldResponseModel->fresh()->customFieldResponses);
         $this->assertEquals('Best Rapper Alive', $customFieldResponseModel->fresh()->customFieldResponses->first()->value_str);
     }
+
+    /** @test */
+    public function whereField_method_allows_filtering_responses()
+    {
+        $customFieldModel = HasCustomFieldsModel::create();
+        $firstResponseModel = HasCustomFieldResponsesModel::create();
+        $secondResponseModel = HasCustomFieldResponsesModel::create();
+
+        $firstField = CustomField::create([
+            'model_id' => $customFieldModel->id,
+            'model_type' => get_class($customFieldModel),
+            'type' => 'text',
+            'subtype' => 'email',
+            'required' => false,
+            'answers' => json_encode(['Boom', 'Bap']),
+            'title' => 'Tha Carter II',
+            'description' => 'Lil Wayne',
+            'order' => '1',
+        ]);
+
+        $firstResponse = CustomFieldResponse::make([
+            'model_id' => $firstResponseModel->id,
+            'model_type' => get_class($firstResponseModel),
+            'field_id' => $firstField->id,
+            'value_str' => 'Hit Em Up',
+        ]);
+
+        $secondResponse = CustomFieldResponse::make([
+            'model_id' => $secondResponseModel->id,
+            'model_type' => get_class($secondResponseModel),
+            'field_id' => $firstField->id,
+            'value_str' => 'Best Rapper Alive',
+        ]);
+
+        $firstResponseModel->customFieldResponses()->save($firstResponse);
+        $secondResponseModel->customFieldResponses()->save($secondResponse);
+
+        $this->assertCount(1, CustomFieldResponseModel::whereField($firstField, 'Hit Em Up')->get());
+        $this->assertEquals($firstResponse->id, CustomFieldResponseModel::whereField($firstField, 'Hit Em Up')->first()->id);
+
+        $this->assertCount(1, CustomFieldResponseModel::whereField($firstField, 'Best Rapper Alive')->get());
+        $this->assertEquals($secondResponse->id, CustomFieldResponseModel::whereField($firstField, 'Best Rapper Alive')->first()->id);
+    }
 }
