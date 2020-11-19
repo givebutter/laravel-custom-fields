4fdcd22ab13f009e7949bbb5996b26965f5367ec
Mark Sitko <mark@sitko-designing.de>
2020-11-19 21:44:58 +0100

    Fix and improve service provider

--- src/LaravelCustomFieldsServiceProvider.php
+++ src/LaravelCustomFieldsServiceProvider.php
@@ -8,14 +8,28 @@ class LaravelCustomFieldsServiceProvider extends ServiceProvider
 {
     public function boot()
     {
-        $this->publishes([
-            __DIR__ . '/../config/custom-fields.php' => config_path('custom-fields.php'),
-        ]);
-
-        if (!class_exists('CreateCustomFieldsTables')) {
+        /*
+        * Optional methods to load your package assets
+        */
+        if ($this->app->runningInConsole()) {
             $this->publishes([
-                __DIR__ . '/../database/migrations/create_custom_fields_tables.php.stub' => database_path('migrations/' . date('Y_m_d_His', time()) . '_create_custom_fields_tables.php'),
-            ], 'migrations');
+                __DIR__.'/../config/custom-fields.php' => config_path('custom-fields.php'),
+            ], 'config');
+
+            if (! class_exists('CreateCustomFieldsTables')) {
+                $this->publishes([
+                    __DIR__.'/../database/migrations/create_custom_fields_tables.php.stub' => database_path('migrations/'.date('Y_m_d_His', time()).'_create_custom_fields_tables.php'),
+                ], 'migrations');
+            }
         }
+    }
+
+    /**
+     * Register the application services.
+     */
+    public function register()
+    {
+        // Automatically apply the package configuration
+        $this->mergeConfigFrom(__DIR__.'/../config/custom-fields.php', 'custom-fields');
     }
 }
